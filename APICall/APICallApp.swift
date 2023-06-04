af99ab4b4ddaeede722f62cc4dc8c6d633590807
boztopuz <boztopuz@gmail.com>
2023-06-04 20:47:16 +0300

    Initial Commit

---
+++ APICall/APICallApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallApp.swift
+//  APICall
+//
+//  Created by Burak Öztopuz on 4.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct APICallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
